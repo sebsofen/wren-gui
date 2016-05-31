44032b753be84f43fe71b6aa4c62920f3dfac514
Sebastian Horwege <sebastian.horwege@stud.uni-goettingen.de>
2016-06-01 00:13:56 +0200

    hide map if not needed

--- app/post-detail.component.ts
+++ app/post-detail.component.ts
@@ -5,6 +5,12 @@ import {PostMetadata, Post, PostAsm, BlogService}   from './blog.service';
 import {ROUTER_DIRECTIVES,RouteConfig,RouteParams, Router} from 'angular2/router';
 import {MarkdownService} from './markdown.service';
 import {Map, TileLayer} from 'leaflet';
+import { Http, Response, Headers } from 'angular2/http';
+
+export class GeoFile {
+  constructor(public path: string, public name: string) { }
+}
+
 
 @Component({
   templateUrl: 'tmpl/post.html',
@@ -12,15 +18,18 @@ import {Map, TileLayer} from 'leaflet';
   directives: [ROUTER_DIRECTIVES],
 
 })
-
 export class PostDetailComponent implements OnInit{
   post: PostAsm;
+  geofiles: GeoFile[] = [];
   private md: MarkdownService;
-
+  mymap: Map;
+  showmap: Boolean = true;
   @ViewChild('map') mapcanvas:ElementRef;
+  geoJson;
 
 
   constructor (
+    private _http: Http,
      private _router:Router,
      private _routeParams:RouteParams,
      private _service:BlogService,
@@ -34,15 +43,41 @@ export class PostDetailComponent implements OnInit{
  ngOnInit() {
 
    let slug = this._routeParams.get('slug');
-   this._service.getPostBySlug(slug).subscribe(f => this.post = f);
+   this._service.getPostBySlug(slug).subscribe(f => {
+     this.post = f
+     //find map tag
+     var myRegexp = /\[map file=\"(.*)\" (.*)\]/g;
+     var match = myRegexp.exec(this.post.post.content);
+     if (match) {
+
+     console.log(match[1]);
+     this.post.post.content = this.post.post.content.replace(match[0],"");
+     this.geofiles.push(new GeoFile("http://localhost:9000/v1/trailmagic/static/" + this.post.metadata.slug + "/" + match[1], match[2]));
+     console.log(this.geofiles);
+
+     this._http.get("http://localhost:9000/v1/trailmagic/static/" + this.post.metadata.slug + "/" + match[1]).subscribe(k => {
+       var group = L.geoJson(k.json());
+       group.addTo(this.mymap);
+       this.mymap.fitBounds(group.getBounds());
+
+     });
+
+   }else{
+     this.showmap = false;
+   }
+   });
 
 
 
 
  }
  ngAfterViewInit() {
+     this.mymap =  L.map(this.mapcanvas.nativeElement).setView([51.505, -0.09], 13);
+    L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+    }).addTo(this.mymap);
 
-   var mymap = L.map(this.mapcanvas.nativeElement);
 
  }
 
